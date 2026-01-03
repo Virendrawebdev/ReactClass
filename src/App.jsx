f5ac19b00081dd8c3b237cbedd6845ab5c3b630b
Virendrawebdev <virendra052001@gmail.com>
2026-01-03 15:50:39 +0530

    useState

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [num, setNum] = useState(0)
+  
+  return (
+    <div>
+      <h1>{num}</h1>
+      <button onClick={()=>{
+         setNum(num +1)
+      }
+       }>increase by 1</button>
+       <button onClick={()=>{
+         setNum(num +5)
+      }
+       }>increase by 5</button>
+    </div>
+  )
+}
+
+export default App
