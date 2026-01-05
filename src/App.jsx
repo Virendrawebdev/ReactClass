61f6f1788dfb7fb53a34f513de0cee2b21bf1b3f
Virendrawebdev <virendra052001@gmail.com>
2026-01-05 17:42:50 +0530

    Two way binding

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+let h1=document.querySelector('h1')
 
 const App = () => {
+   const [name, setName] = useState('')
+   const [email, setEmail] = useState('')
+   const [allUsers, setAllusers] = useState([])
   const submitHandler=(e)=>{
     e.preventDefault()
-    console.log('Form Submitted')
+    const oldAllUsers =[...allUsers]
+    oldAllUsers.push({name,email})
+    // h1.innerHTML=newAllUsers
+    console.log(oldAllUsers)
+    setAllusers(oldAllUsers)
+    setName('')
+    setEmail('')
   }
+
  
   return (
     <div>
     <form onSubmit={(e)=>{
       submitHandler(e)
     }}>
-    <input type='text' placeholder='Enter your name '/>
+    <input type='text' placeholder='Enter your name' required value={name}  onChange={(e)=>{
+      setName(e.target.value)
+    }} />
+    <input type='text' placeholder='Enter your email' required value={email}  onChange={(e)=>{
+      setEmail(e.target.value)
+    }} />
     <button>Submit</button>
     </form> 
+    {allUsers.map((e,idx)=>{
+      return <div key={idx}>
+        <h1>{e.name}</h1>
+        <h1>{e.email}</h1>
+      </div>
+    })}
     </div>
   )
 }
